edb24f28c3d56df033c00e439fe430d5f77be3ec
sgca85 <162612350+sgca85@users.noreply.github.com>
2025-05-19 09:08:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Groot Chatbot ---");
+string pergunta = "";
+while (true)
+
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine();
+    if (pergunta == "Tchau")
+
+    {
+        Console.WriteLine("             Resposta: Eu sou Groot!");        
+        break;
+    }
+    
+    Console.WriteLine("                 Resposta: Eu sou Groot.");
+}
